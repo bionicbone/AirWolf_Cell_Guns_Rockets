29b8bd18ac24ccc3f7fccdb12fa5483a001326a0
Kevin Guest <github@bionicbone.plus.com>
2024-05-01 07:32:05 +0100

    Add project files.

---
+++ AirWolf_Cell_Guns_Rockets.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		AirWolf_Cell_Guns_Rockets.ino
+ Created:	5/1/2024 7:27:20 AM
+ Author:	Bionicbone (YouTube / RC Groups)
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <ESP32_ESC_High_Resolution_Driver.h>
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
